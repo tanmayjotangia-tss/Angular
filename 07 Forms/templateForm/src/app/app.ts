4f04037cb7bbba79388a01ed5afb562839209509
tanmayjotangia-tss <tanmay.jotangia@tssconsultancy.com>
2026-04-13 17:03:35 +0530

    Fixed Form Data Manipulation

--- 07 Forms/templateForm/src/app/app.ts
+++ 07 Forms/templateForm/src/app/app.ts
@@ -11,14 +11,26 @@ export class App {
   name:string = '';
   email:string = '';
   country:string = '';
-genders: string[] = ["Male", "Female"];
-genderSelected: string = '';
-  onSubmit(form: NgForm) {
-    this.name = form.value.name;
-    this.email = form.value.email;
-    this.country = form.value.country;
-    this.genderSelected = form.value.gender;
+  genders: string[] = ["Male", "Female"];
+  genderSelected: string = '';
+  hobbies: string[] = [];
 
-    console.log('Form submitted with values:', form.value);
+  onHobbyChange(event: any) {
+    const value = event.target.value;
+
+    if (event.target.checked) {
+      this.hobbies.push(value);
+    } else {
+      this.hobbies = this.hobbies.filter(h => h !== value);
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    const formData = {
+      ...form.value,
+      hobbies: this.hobbies
+    };
+
+    console.log('Form submitted with values:', formData);
   }
 }

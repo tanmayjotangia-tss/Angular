ef561261d3f06efc973453ceb68fa8d79e0953b5
tanmayjotangia-tss <tanmay.jotangia@tssconsultancy.com>
2026-04-17 15:37:48 +0530

    Admin Routing Added

--- 11. Modular Routing/modularRoutingDemo/src/app/admin/admin-routing-module.ts
+++ 11. Modular Routing/modularRoutingDemo/src/app/admin/admin-routing-module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Dashboard } from './component/dashboard/dashboard';
+import { ApproveBank } from './component/approve-bank/approve-bank';
+import { ApproveClient } from './component/approve-client/approve-client';
+import { BankList } from './component/bank-list/bank-list';
+import { ClientList } from './component/client-list/client-list';
 
 const routes: Routes = [
-  {path:"",component:Dashboard}
+  {path:"",component:Dashboard},
+  {path:"approve-bank", component:ApproveBank},
+  {path:"approve-client", component:ApproveClient},
+  {path:"bank-list", component:BankList},
+  {path:"client-list", component:ClientList}
 ];
 
 @NgModule({
